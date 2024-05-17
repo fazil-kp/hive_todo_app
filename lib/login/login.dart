c9a220818315d081a620fb2a4871932a045da542
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-05-17 23:38:51 +0530

    update 8 files

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,103 +1,35 @@
-import 'package:core/helpers/core_helpers.dart';
-import 'package:core/widgets/core_button.dart';
-import 'package:core/widgets/core_text_field.dart';
-import 'package:flutter/gestures.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nb_utils/nb_utils.dart';
-import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final respo = RespoHelper.isDesktop(context);
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Row(
         children: [
-          if (respo) const SideBarVideo(),
-          Expanded(
-            flex: 2,
-            child: Container(
-              decoration: respo ? null : BoxDecoration(color: blackColor.withOpacity(.9), image: const DecorationImage(image: AssetImage("assets/images/testimg3.jpg"), fit: BoxFit.cover, opacity: .3)),
-              child: Padding(
-                padding: EdgeInsets.all(respo ? 20 : 12),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      elevation: respo ? 0 : 80,
-                      color: respo ? transparentColor : whiteColor.withOpacity(.2),
-                      child: SizedBox(
-                        height: respo ? 400 : 400,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Center(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                CustomTextField(
-                                  width: respo ? 370 : double.infinity,
-                                  hintText: "Email",
-                                  height: respo ? 50 : 65,
-                                  onEditingComplete: () {
-                                    FocusScope.of(context).nextFocus();
-                                  },
-                                ),
-                                40.height,
-                                CustomTextField(
-                                  width: respo ? 370 : double.infinity,
-                                  hintText: "Password",
-                                  height: respo ? 50 : 65,
-                                  onEditingComplete: () async {
-                                    // context.goNamed(RoutePathCore.todo.name);
-                                  },
-                                  suffix: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.visibility_off),
-                                    iconSize: 20,
-                                  ),
-                                ),
-                                40.height,
-                                SizedBox(
-                                  width: respo ? 370 : double.infinity,
-                                  height: respo ? 50 : 65,
-                                  child: CustomButton(
-                                    onTap: () {},
-                                    text: 'Login',
-                                  ),
-                                ),
-                                15.height,
-                                Center(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      style: Theme.of(context).textTheme.labelMedium?.copyWith(color: respo ? darkGrey : whiteColor.withOpacity(.7).withOpacity(.7), fontSize: 14, fontWeight: FontWeight.bold),
-                                      children: [
-                                        const TextSpan(text: "Don't have an account? "),
-                                        TextSpan(
-                                          text: 'Contact Us',
-                                          style: Theme.of(context).textTheme.labelMedium?.copyWith(color: Theme.of(context).primaryColor, fontSize: 14, fontWeight: FontWeight.bold),
-                                          recognizer: TapGestureRecognizer()..onTap = () {},
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    10.height,
-                  ],
-                ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AssetPlayerWidget(),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 400,
+                height: 400,
+                color: Colors.red,
               ),
-            ),
+            ],
           ),
         ],
       ),
@@ -105,65 +37,63 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
-class SideBarVideo extends StatefulWidget {
-  const SideBarVideo({Key? key}) : super(key: key);
+class AssetPlayerWidget extends StatefulWidget {
+  const AssetPlayerWidget({Key? key}) : super(key: key);
 
   @override
-  _SideBarVideoState createState() => _SideBarVideoState();
+  _AssetPlayerWidgetState createState() => _AssetPlayerWidgetState();
 }
 
-class _SideBarVideoState extends State<SideBarVideo> {
-  final videoUrl = "https://youtu.be/m9coOXt5nuw?si=1TNnLqng0q6_t9o1";
-  late YoutubePlayerController _youtubePlayerController;
+class _AssetPlayerWidgetState extends State<AssetPlayerWidget> {
+  final asset = 'assets/videos/loginvideo.mp4';
+  late VideoPlayerController controller;
 
   @override
   void initState() {
     super.initState();
-    _youtubePlayerController = YoutubePlayerController(
-      initialVideoId: YoutubePlayer.convertUrlToId(videoUrl)!,
-      flags: const YoutubePlayerFlags(
-        autoPlay: false,
-        mute: false,
-      ),
-    )..addListener(() {
-        setState(() {});
-      });
+    controller = VideoPlayerController.asset(asset)
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => controller.play());
   }
 
   @override
   void dispose() {
-    _youtubePlayerController.dispose();
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 2,
-      child: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: YoutubePlayer(
-          controller: _youtubePlayerController,
-          showVideoProgressIndicator: true,
-          progressIndicatorColor: Colors.red,
-          onReady: () {
-            _youtubePlayerController.play();
-          },
-          bottomActions: [
-            CurrentPosition(),
-            ProgressBar(
-              isExpanded: true,
-              colors: ProgressBarColors(
-                playedColor: Colors.red,
-                handleColor: Colors.red,
-              ),
-            ),
-            PlaybackSpeedButton(),
-            RemainingDuration(),
-            FullScreenButton(),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
+      child: SizedBox(
+        height: context.height() * 0.65,
+        width: context.width() * 0.65,
+        child: VideoPlayerWidget(controller: controller),
       ),
     );
   }
+}
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const VideoPlayerWidget({Key? key, required this.controller}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => controller.value.isInitialized
+      ? buildVideo()
+      : SizedBox(
+          height: 200,
+          child: Center(child: CircularProgressIndicator()),
+        );
+
+  Widget buildVideo() => Stack(
+        children: <Widget>[
+          buildVideoPlayer(),
+        ],
+      );
+
+  Widget buildVideoPlayer() => VideoPlayer(controller);
 }
