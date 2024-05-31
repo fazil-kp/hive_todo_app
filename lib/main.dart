fec29d22bff4c5a941d6c7b893c5063bac910bfe
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-05-31 22:11:26 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MainProject extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: coreRoute,
