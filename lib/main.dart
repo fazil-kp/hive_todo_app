6b5a06219d464d235cd004783d56645fb53ad2ce
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-06-03 21:47:09 +0530

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,11 @@ void main() async {
 
 class MainProject extends ConsumerWidget {
   const MainProject({super.key});
+
+
+
+
+  
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
