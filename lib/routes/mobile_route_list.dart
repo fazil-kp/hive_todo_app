87d57a562f052ceb8edf7492351ab419a65dfd1d
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-07-18 19:06:28 +0530

    update mobile_route_list.dart

--- lib/routes/mobile_route_list.dart
+++ lib/routes/mobile_route_list.dart
@@ -6,14 +6,16 @@ import '../screens/employee/employee_screen.dart';
 import '../screens/student/student_screen.dart';
 import '../screens/test_screens/test2.dart';
 
-List<CoreRouteModel> mobileRouteList = [
-  //! home screen routes...................
 
+List<CoreRouteModel> mobileRouteList = [
+  
+  //! home screen routes...................
+  
   const CoreRouteModel(id: 1, name: 'Student', routeName: 'mob-student', widget: CustomScaffold(childWidget: StudentScreen())),
   const CoreRouteModel(id: 2, name: 'Employee', routeName: 'mob-employee', widget: CustomScaffold(childWidget: EmployeeScreen())),
   const CoreRouteModel(id: 3, name: 'Test Two', routeName: 'mob-test-two', widget: CustomScaffold(childWidget: TestTwo())),
   const CoreRouteModel(id: 4, name: 'Contacts', routeName: 'mob-contact', widget: CustomScaffold(childWidget: ContactScreenScreen())),
 
   //! Profile screen routes...................
-  
+
 ];
