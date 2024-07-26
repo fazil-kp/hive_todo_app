ce83027bd613c243e3b427eabe2fc76b1cc6ac17
Fazil <104255104+fazil-kp@users.noreply.github.com>
2024-07-26 21:05:28 +0530

    update apis.dart

--- lib/api/apis.dart
+++ lib/api/apis.dart
@@ -2,3 +2,12 @@ class Api {
   static String category = "https://api.rentonline.in:3005/client/rent-online/category/get-all";
   static String contact = "http://localhost:5001/api/contacts";
 }
+
+
+
+
+
+
+
+
+
